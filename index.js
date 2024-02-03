3aebf9b0c98c52a9a89f9cc55ca1bfc7d8722b28
정삼복 <68502584+3boku@users.noreply.github.com>
2024-02-04 00:40:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const app = express();
 app.post("/login", (req, res) => {
     return res.json({ status: true, message: "login sucess." });
 });
+
+app.get("/logout", (req, res) => {
+    return res.json({ status: true, message: "logout sucess." });
+});
+
 app.listen(3000, () => console.log("server is running.."));

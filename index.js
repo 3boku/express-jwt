dcd4a9799e90c6c448dbf5d3e4c18936fd421e32
정삼복 <68502584+3boku@users.noreply.github.com>
2024-02-05 19:47:10 +0900

    change

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+require("dotenv");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
 
-app.post("/login", (req, res) => {
-    return res.json({ status: true, message: "login sucess." });
-});
+// middleware
+app.use(express.json());
 
-app.get("/logout", (req, res) => {
-    return res.json({ status: true, message: "logout sucess." });
+// routes
+// registeration
+
+//login
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username === "raj" && password === "123456") {
+        const access_token = jwt.sign({ sub: username }, "");
+        return res.json({ status: true, message: "login sucess" });
+    }
+
+    return res.status(401).json({ status: true, message: "login faild" });
 });
 
 app.listen(3000, () => console.log("server is running.."));
